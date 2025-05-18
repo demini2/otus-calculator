c6fdb3dc3147666bbc27fb753047da172af1f24c
mini <demini232@mail.ru>
2025-05-18 14:17:05 +0300

    main

---
+++ src/Calculator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Demini2\OtusCalculator;
+
+/**
+ * Клас калькулятора
+ */
+class Calculator
+{
+    /**
+     * Получить сумму двух чисел
+     *
+     * @param int|float $a
+     * @param int|float $b
+     * @return float|int|string
+     */
+    public function getSum(int|float $a, int|float $b): float|int|string
+    {
+        if (!is_int($a) && !is_float($a)) {
+
+            return 'Переменная $a должна быть числом';
+        }
+
+        if (!is_int($b) && !is_float($b)) {
+
+            return 'Переменная $a должна быть числом';
+        }
+
+        return $a + $b;
+    }
+}
